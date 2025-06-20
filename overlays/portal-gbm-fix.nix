41578e5459aed5a20e5da550a72cf6da9e7fd44c
Redm00us <krokismau@icloud.com>
2025-06-20 19:29:23 +0300

    debug: add preConfigure hook for PKG_CONFIG_PATH and pkg-config gbm check

--- overlays/portal-gbm-fix.nix
+++ overlays/portal-gbm-fix.nix
@@ -2,7 +2,10 @@ self: super: {
   xdg-desktop-portal-hyprland = super.xdg-desktop-portal-hyprland.overrideAttrs (old: {
     buildInputs = (old.buildInputs or []) ++ [ super.mesa super.libgbm ];
     nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ super.pkg-config ];
-    PKG_CONFIG_PATH = "${super.libgbm}/lib/pkgconfig";
-    configureFlags = (old.configureFlags or []) ++ [ "PKG_CONFIG_PATH=${super.libgbm}/lib/pkgconfig" ];
+    preConfigure = ''
+      export PKG_CONFIG_PATH="${super.libgbm}/lib/pkgconfig:$PKG_CONFIG_PATH"
+      echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
+      pkg-config --list-all | grep gbm || true
+    '';
   });
 }
