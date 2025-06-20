5ecaed0e174e9b4bc6abfcf23608eac26f601dd4
Redm00us <krokismau@icloud.com>
2025-06-20 19:26:47 +0300

    fix: patch PKG_CONFIG_PATH for xdg-desktop-portal-hyprland build

--- overlays/portal-gbm-fix.nix
+++ overlays/portal-gbm-fix.nix
@@ -2,5 +2,7 @@ self: super: {
   xdg-desktop-portal-hyprland = super.xdg-desktop-portal-hyprland.overrideAttrs (old: {
     buildInputs = (old.buildInputs or []) ++ [ super.mesa super.libgbm ];
     nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ super.pkg-config ];
+    PKG_CONFIG_PATH = "${super.libgbm}/lib/pkgconfig";
+    configureFlags = (old.configureFlags or []) ++ [ "PKG_CONFIG_PATH=${super.libgbm}/lib/pkgconfig" ];
   });
 }
